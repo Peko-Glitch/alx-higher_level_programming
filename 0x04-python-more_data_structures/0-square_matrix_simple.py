0885b40ac4bd7dee2d8e1755da57187eef1fcc60
Peko-Glitch <mrgetpyked777@gmail.com>
2024-03-22 14:53:35 -0700

    task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,6 @@ def square_matrix_simple(matrix=[]):
     cols = len(matrix[0]) if matrix else 0
 
     # Create a new matrix filled with squares of elements
-    squared_matrix = [[matrix[i][j] ** 2 for j in range(cols)] for i in range(rows)]
+    sqrd_mtrx = [[matrix[i][j] ** 2 for j in range(cols)] for i in range(rows)]
 
-    return squared_matrix
-
+    return sqrd_mtrx
