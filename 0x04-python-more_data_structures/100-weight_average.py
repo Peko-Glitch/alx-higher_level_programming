6caec6f37f4156aaf8965a1fee0181dbae538f4b
Peko-Glitch <mrgetpyked777@gmail.com>
2024-03-22 14:58:02 -0700

    task100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    # Check if the list is empty
+    if not my_list:
+        return 0
+    
+    # Initialize variables for sum of products and sum of weights
+    sum_product = 0
+    sum_weights = 0
+    
+    # Calculate sum of products and sum of weights
+    for score, weight in my_list:
+        sum_product += score * weight
+        sum_weights += weight
+    
+    # Calculate the weighted average
+    weighted_avg = sum_product / sum_weights
+    
+    return weighted_avg
